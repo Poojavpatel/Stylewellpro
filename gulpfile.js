2b2645333bace4d810c9a77ab861c746fc4e5d5e
Pooja patel <poojavpatel71@gmail.com>
2018-08-16 01:15:11 +0530

    Finalllyyyy....done with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@
 // sudo npm i gulp-cli
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
-var csso = require('gulp-csso');
-const minify = require('gulp-minify');
+// var csso = require('gulp-csso');
+let cleanCSS = require('gulp-clean-css');
+// const minify = require('gulp-minify');
 var runSequence = require('run-sequence');
-// var uglify = require('gulp-uglify');
-var smushit = require('gulp-smushit');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+// var smushit = require('gulp-smushit');
 
 gulp.task('sayhello', function() {
     console.log('Hello');
@@ -14,7 +16,7 @@ gulp.task('sayhello', function() {
 
 // Gulp task to minify HTML files
 gulp.task('minifyhtml', function() {
-    return gulp.src(['./*.html'])
+    return gulp.src(['./index.html','./form.html'])
       .pipe(htmlmin({
         collapseWhitespace: true,
         removeComments: true
@@ -23,24 +25,40 @@ gulp.task('minifyhtml', function() {
   });
 
 // Gulp task to minify CSS files
-gulp.task('minifycss', function () {
+// gulp.task('minifycss', function() {
+//     return gulp.src('./css/*.css')
+//       // Minify the file
+//       .pipe(csso())
+//       // Output
+//       .pipe(gulp.dest('./build/css/'));
+//   });
+gulp.task('minifycss', () => {
     return gulp.src('./css/*.css')
-      // Minify the file
-      .pipe(csso())
-      // Output
+      .pipe(cleanCSS({compatibility: '*'}))
       .pipe(gulp.dest('./build/css/'));
-  });
+});
 
 // Gulp task to minify JavaScript files
-gulp.task('minifyjs', function() {
+// gulp.task('minifyjs', function() {
     // return gulp.src('./script.js')
     //   // Minify the file
     //   .pipe(uglify())
     //   // Output
     //   .pipe(gulp.dest('./build'));
-    gulp.src('./js/script.js')
-    .pipe(minify())
-    .pipe(gulp.dest('./build/js/'));
+// gulp.task('minifyjs', function(done) {
+//     gulp.src('./js/script.js')
+//     .pipe(minify())
+//     .pipe(gulp.dest('./build/js/'))
+//     done();
+//   });
+  gulp.task('minifyjs', function (cb) {
+    pump([
+          gulp.src('./js/script.js'),
+          uglify(),
+          gulp.dest('./build/js/')
+      ],
+      cb
+    );
   });
 
 // //Optimizing images
