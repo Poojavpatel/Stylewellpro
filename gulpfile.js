a651db290809b0d7d5cec6e706f0a582173a7c30
Pooja patel <poojavpatel71@gmail.com>
2018-08-15 23:59:10 +0530

    changed gulpfile.js | not using imagemin as it has vulnerability

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,15 @@
 /*jshint esversion: 6 */
 var gulp = require('gulp');
-var csso = require('gulp-csso');
 var htmlmin = require('gulp-htmlmin');
+var csso = require('gulp-csso');
+const minify = require('gulp-minify');
 var runSequence = require('run-sequence');
 // var uglify = require('gulp-uglify');
-const minify = require('gulp-minify');
 var smushit = require('gulp-smushit');
 
 gulp.task('sayhello', function() {
     console.log('Hello');
 });
-
-// Gulp task to minify CSS files
-gulp.task('minifycss', function () {
-    return gulp.src('./style.css')
-      // Minify the file
-      .pipe(csso())
-      // Output
-      .pipe(gulp.dest('./build'));
-  });
-
-// Gulp task to minify JavaScript files
-gulp.task('minifyjs', function() {
-    // return gulp.src('./script.js')
-    //   // Minify the file
-    //   .pipe(uglify())
-    //   // Output
-    //   .pipe(gulp.dest('./build'));
-    gulp.src('./script.js')
-    .pipe(minify())
-    .pipe(gulp.dest('./build'));
-  });
 
 // Gulp task to minify HTML files
 gulp.task('minifyhtml', function() {
@@ -42,19 +21,39 @@ gulp.task('minifyhtml', function() {
       .pipe(gulp.dest('./build'));
   });
 
-//Optimizing images
-gulp.task('minifyimg', function () {
-    return gulp.src('./img/*.{jpg,png}')
-        .pipe(smushit())
-        .pipe(gulp.dest('./build/img'));
-});
+// Gulp task to minify CSS files
+gulp.task('minifycss', function () {
+    return gulp.src('./css/*.css')
+      // Minify the file
+      .pipe(csso())
+      // Output
+      .pipe(gulp.dest('./build/css'));
+  });
+
+// Gulp task to minify JavaScript files
+gulp.task('minifyjs', function() {
+    // return gulp.src('./script.js')
+    //   // Minify the file
+    //   .pipe(uglify())
+    //   // Output
+    //   .pipe(gulp.dest('./build'));
+    gulp.src('./js/script.js')
+    .pipe(minify())
+    .pipe(gulp.dest('./build/js'));
+  });
+
+// //Optimizing images
+// gulp.task('minifyimg', function () {
+//     return gulp.src('./img/*.{jpg,png}')
+//         .pipe(smushit())
+//         .pipe(gulp.dest('./build/img'));
+// });
 
 // Gulp task to minify all files
 gulp.task('rockit', function () {
     runSequence(
       'minifyhtml',
       'minifycss',
-      'minifyimg',
       'minifyjs'      
     );
   });
